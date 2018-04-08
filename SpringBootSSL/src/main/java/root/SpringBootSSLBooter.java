d7e7f37914b500fab7b0f167aa49dcb1fcac5722
Kaushi <Kaushi@Kaushi-PC>
2018-04-09 01:48:20 +0530

    added comment for test

--- SpringBootSSL/src/main/java/root/SpringBootSSLBooter.java
+++ SpringBootSSL/src/main/java/root/SpringBootSSLBooter.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringBootSSLBooter {
 
+	//start point of the application
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootSSLBooter.class, args);
 	}
